3ca6b2ce3d2590758b76c10c4700c0366b90fff4
James Heathcote <james.h@biffbangpow.com>
2019-03-19 17:36:10 +0000

    Adding new title field in

--- src/Controllers/JobBoardPageController.php
+++ src/Controllers/JobBoardPageController.php
@@ -5,6 +5,8 @@ namespace BiffBangPow\SilverStripeREJB\Controllers;
 use SilverStripe\ORM\FieldType\DBHTMLText;
 use SilverStripe\Control\HTTPRequest;
 use PageController;
+use Stringy\Stringy;
+use SilverStripe\Core\Config\Config;
 
 /**
  * @package BiffBangPow\SilverStripeREJB
@@ -17,6 +19,17 @@ class JobBoardPageController extends PageController
      */
     public function index(HTTPRequest $request)
     {
-        return $this->renderWith(['JobBoard', 'Page'], ['MetaTitle' => 'Jobs']);
+        $metaTitle = 'Jobs';
+
+        $jobBoardURLPath = Config::inst()->get('BiffBangPow\SilverStripeREJB\SilverstripeREJB', 'job_board_url_path');
+
+        if ($request->getURL() !== $jobBoardURLPath) {
+            $urlParts = explode('/', $request->getURL());
+            $title = str_replace('-', ' ', end($urlParts));
+            $titleString = new Stringy($title);
+            $metaTitle = $titleString->titleize()->__toString();
+        }
+
+        return $this->renderWith(['JobBoard', 'Page'], ['MetaTitle' => $metaTitle]);
     }
 }
